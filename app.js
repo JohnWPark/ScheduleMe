0d607541a5e9a61fff27229e4e3e2cbccc84c2b9
John Park <jwpark417@gmail.com>
2016-07-14 17:15:22 -0400

    Add firebase and dependencies via npm and bower

---
+++ app.js
@@ -0,0 +1,7 @@
+// create angular module and inject firebase
+angular.module('scheduleApp', ['firebase'])
+
+// create main controller and get access to firebase
+.controller('mainController', function($scope, $firebase) {
+  
+})
